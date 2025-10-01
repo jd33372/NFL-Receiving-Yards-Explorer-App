9318650a71760c1bba1219284b11475771167e34
jd33372 <jamesdiluca58@gmail.com>
2025-09-30 22:37:57 -0400

    Refactor NFL wide receiver app code structure

--- nfl_wide_receiver_app.py
+++ nfl_wide_receiver_app.py
@@ -94,7 +94,7 @@ if st.sidebar.button('Generate Scatter Plot'):
 # Get average yards by position
 
 pos_avg = df_selected_team.groupby('Pos')['Yds'].mean().reset_index().round(2)
-pos_avg = df_selected_team.sort_values(by='Yds', ascending=False)
+pos_avg = pos_avg.sort_values(by='Yds', ascending=False)
 
 if st.button('Average Receiving Yards by Position'):
     st.header('Average Receiving Yards by Position for the Year of ' + str(selected_year))
@@ -137,6 +137,7 @@ if st.button('Show Player Grade'):
     player = player_data[['Rank', 'Player', 'Age', 'Team', 'Pos', 'Rec', 'Yds', 'Receiving Grade']].round(2)
 
     st.write(player)
+
 
 
 
